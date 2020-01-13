9099b2d9ddebb7d8bcb536a548455d35c6862009
Matheus-Godinho <matheus.brc22@gmail.com>
2020-01-13 01:40:46 -0300

    adicionado o preenchimento de dados do cliente

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,35 @@
 package application;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Client;
+
 public class Program {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws ParseException {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		SimpleDateFormat sdf;
+		String name, email;
+		Date birthDate;
+		Client client;
+		
+		sdf = new SimpleDateFormat();		
+		System.out.printf("Enter client data:%n");
+		System.out.printf("Name: ");
+		name = sc.nextLine();
+		System.out.printf("Email: ");
+		email = sc.nextLine();
+		System.out.printf("Birth date (DD/MM/YYYY): ");
+		birthDate = sdf.parse(sc.next());
+		client = new Client(name, email, birthDate);
+		
+		sc.close();
 
 	}
 
